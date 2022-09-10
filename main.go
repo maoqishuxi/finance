58ff88b767482871a5a5b31b7d066e78d72544f6
root <root@maoshu.localdomain>
2022-09-11 01:56:41 +0800

    插入数据功能完成

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"finance/writeToDatabase"
 	"fmt"
@@ -11,7 +10,7 @@ import (
 	"regexp"
 )
 
-func fetchUrl(code string, page int) ([]map[string][]byte, error) {
+func fetchUrl(code string, page int) ([]writeToDatabase.Item, error) {
 	url := "https://fundf10.eastmoney.com/F10DataApi.aspx?type=lsjz&code=%s&page=%d&per=20"
 	response, err := http.Get(fmt.Sprintf(url, code, page))
 	if err != nil {
@@ -29,22 +28,23 @@ func fetchUrl(code string, page int) ([]map[string][]byte, error) {
 	express := `<td[a-z\s=\']*?>([0-9\.%-]+)</td>`
 	data := regexp.MustCompile(express).FindAllSubmatch(body, -1)
 
-	result := make([]map[string][]byte, 0)
+	result := make([]writeToDatabase.Item, 0)
 
 	for i := 0; i < len(data); i += 4 {
-		result = append(result, map[string][]byte{
-			"time":   data[i][1],
-			"valueV": data[i+1][1],
-			"valueC": data[i+2][1],
-			"gain":   data[i+3][1],
+		result = append(result, writeToDatabase.Item{
+			Id:     i / 4,
+			Time:   data[i][1],
+			ValueV: data[i+1][1],
+			ValueC: data[i+2][1],
+			Gain:   data[i+3][1],
 		})
 	}
 
 	return result, nil
 }
 
-func getData(code string, page int) []map[string][]byte {
-	var result []map[string][]byte
+func getData(code string, page int) []writeToDatabase.Item {
+	var result []writeToDatabase.Item
 	for i := 0; i < 5; i++ {
 		ret, err := fetchUrl(code, page)
 		if err == nil {
@@ -60,25 +60,24 @@ func getData(code string, page int) []map[string][]byte {
 func main() {
 	//code := "000942"
 	//page := 1
-
-	fmt.Println("welcome to you")
-
-	//data := getData(code, page)
 	db, err := sql.Open("sqlite3", "./data/file.db")
 	if err != nil {
 		log.Println(err)
 	}
 	defer db.Close()
 
-	var ctx context.Context
-	var data = map[string][]byte{
-		"id":     []byte("0"),
-		"time":   []byte("2022-09-10"),
-		"valueV": []byte("2"),
-		"valueC": []byte("2"),
-		"gain":   []byte("1.0"),
-	}
-
+	//data := getData(code, page)
 	//writeToDatabase.CreateTable(db, "file")
-	writeToDatabase.InsertData(db, ctx, data)
+	//for _, item := range data {
+	//	writeToDatabase.InsertData(db, item)
+	//}
+
+	//result := writeToDatabase.QueryData(db, 10)
+	//for _, item := range result {
+	//	fmt.Println(item.Id)
+	//	fmt.Println(string(item.ValueV))
+	//	fmt.Println(string(item.Gain))
+	//}
+	//id := writeToDatabase.QueryID(db)
+	//fmt.Println(id)
 }
--- writeToDatabase/writeToDatabase.go
+++ writeToDatabase/writeToDatabase.go
@@ -1,7 +1,6 @@
 package writeToDatabase
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -9,14 +8,22 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Item struct {
+	Id     int
+	Time   []byte
+	ValueV []byte
+	ValueC []byte
+	Gain   []byte
+}
+
 func CreateTable(db *sql.DB, tableName string) {
 	sqlStmt := `
 	CREATE TABLE IF NOT EXISTS %s (
 	    id INTEGER,
 	    time TEXT PRIMARY KEY,
-	    valueV REAL,
-	    valueC REAL,
-	    gain REAL
+	    valueV TEXT,
+	    valueC TEXT,
+	    gain TEXT
 	)
 `
 	_, err := db.Exec(fmt.Sprintf(sqlStmt, tableName))
@@ -27,32 +34,47 @@ func CreateTable(db *sql.DB, tableName string) {
 	fmt.Printf("创建%s表成功\n", tableName)
 }
 
-func InsertData(db *sql.DB, ctx context.Context, data map[string][]byte) {
-	tx, err := db.BeginTx(ctx, nil)
+func QueryData(db *sql.DB, num int) []Item {
+	rows, err := db.Query("select * from file limit ?", num)
 	if err != nil {
 		log.Println(err)
 	}
-	defer tx.Rollback()
+	defer rows.Close()
 
-	log.Println("开启事务")
+	result := make([]Item, 0)
+	var ret Item
+	for rows.Next() {
+		if err := rows.Scan(&ret.Id, &ret.Time, &ret.ValueV, &ret.ValueC, &ret.Gain); err != nil {
+			log.Printf("query error: %v\n", err)
+		}
+		result = append(result, ret)
+	}
 
-	result, err := tx.ExecContext(ctx, "INSERT INTO file VALUES(?,?,?,?,?)",
-		data["id"], data["time"], data["valueV"], data["valueC"], data["gain"])
+	return result
+}
+
+func QueryID(db *sql.DB) int {
+	var id int
+	row := db.QueryRow("SELECT id FROM file ORDER BY id desc LIMIT 1")
+	if err := row.Scan(&id); err != nil {
+		log.Printf("QueryID: %v\n", err)
+	}
+
+	return id
+}
+
+func InsertData(db *sql.DB, data Item) {
+	result, err := db.Exec("INSERT INTO file VALUES (?,?,?,?,?)",
+		data.Id, data.Time, data.ValueV, data.ValueC, data.Gain)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	id, err := result.LastInsertId()
 	if err != nil {
 		log.Println(err)
 	}
 
-	itemID, err := result.LastInsertId()
-	fmt.Println(itemID)
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Println("插入数据完成，准备提交")
-
-	if err = tx.Commit(); err != nil {
-		log.Println(err)
-	}
-
-	log.Println("提交完成")
+	fmt.Println(id)
 }
